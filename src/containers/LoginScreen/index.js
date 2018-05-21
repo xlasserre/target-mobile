c377e8bb3b59e1835c6890232dfbe17dc6229360
Ximena Lasserre <ximena@Ximenas-Mac-mini.local>
2018-05-21 12:51:17 -0300

    Structure app

---
+++ src/containers/LoginScreen/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import styles from './styles';
+
+class LoginScreen extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <View>Login</View>
+        );
+    }
+}
+
+export default LoginScreen;
